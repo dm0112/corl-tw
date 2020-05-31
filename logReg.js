d305c7f96566b6649212baec98f1eca852448af1
Serban Movila <48479374+serbanmovila@users.noreply.github.com>
2020-05-31 21:13:46 +0300

    Add files via upload

---
+++ logReg.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.getElementById("logFrame").style.display = "flex"
+    document.getElementById("regFrame").style.display = "none"
+}
+
+function showRegister() {
+    document.getElementById("logFrame").style.display = "none"
+    document.getElementById("regFrame").style.display = "flex"
+}
