c1910d4c67a35b05fe38ac223155392c793403fe
lowly <mihaidaniel54@gmail.com>
2020-06-03 21:35:28 +0300

    organizing

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$db = "colr";
-$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-
-?>
